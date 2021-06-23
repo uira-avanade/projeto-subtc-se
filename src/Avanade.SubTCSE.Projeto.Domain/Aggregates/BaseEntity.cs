96fe421cf3552c42f6d484d1eb7aa4441e2dc4a9
Uira Tupinambas Lima <uira.tupinambas.lima@avanade.com>
2021-06-23 09:00:18 -0300

    Dia 07

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates
+{
+    public abstract record BaseEntity<TId>
+    {
+        public TId Id { get; init; }
+    }
+}
--- src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -1,6 +1,6 @@
 ﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
 {
-    public record EmployeeRole
+    public record EmployeeRole : BaseEntity<string>
     {
         public EmployeeRole(string id, string roleName)
         {
@@ -13,8 +13,6 @@
             RoleName = roleName;
         }
 
-        public string Id { get; init; }
-        
         public string RoleName { get; init; }
     }
 }
